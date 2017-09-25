a575ede35fb72e8f62df0654af05a665ad0fd74a
danaolcott <danaolcott@gmail.com>
2017-09-25 15:59:22 -0700

    initial project blink commit

---
+++ source/cprog/eclipse/bbb_blink/main.c
@@ -0,0 +1,19 @@
+/*
+ * main.c
+ *
+ *  Created on: Sep 25, 2017
+ *      Author: danao
+ *
+ *      Simple Hello and Blink GPIO Pin
+ */
+
+#include <stdio.h>
+
+#include "main.h"
+
+int main()
+{
+	printf("Hello\n");
+	return 0;
+}
+
---
+++ source/cprog/eclipse/bbb_blink/main.h
@@ -0,0 +1,13 @@
+/*
+ * main.h
+ *
+ *  Created on: Sep 25, 2017
+ *      Author: danao
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+
+
+#endif /* MAIN_H_ */
