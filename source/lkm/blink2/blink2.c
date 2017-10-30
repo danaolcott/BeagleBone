1eb91210b9b436829d1bf38d564162ecaf4612c8
danaolcott <danaolcott@gmail.com>
2017-10-30 09:25:21 -0700

    update blink2 to use gpio26 and 27

--- source/lkm/blink2/blink2.c
+++ source/lkm/blink2/blink2.c
@@ -5,8 +5,16 @@
 Simple Flashing LED Example using Linux/gpio.h
 
 The purpose of this module is to use /linux/gpio.h
-to toggle an led.  The led will toggle using a workqueue
+to toggle the leds on the beaglebone backpack board
+(see backpack for schematics/pcb express files).  
+GPIO Pins are located on GPIO26 (8-14) and
+GPIO27 (8-17).
+
+The led will toggle using a workqueue
 and posting delayed work.
+
+GPIO26 - Blue Led on the backpack
+GPIO27 - Red Led on the backpack
 
 */
 
@@ -18,18 +26,20 @@ and posting delayed work.
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Dana Olcott");
-MODULE_DESCRIPTION("Simple LED Toggle Example");
+MODULE_DESCRIPTION("Toggle GPIO26 and GPIO27 Example");
 MODULE_VERSION("0.1");
 
 
 
 /////////////////////////////////////////////
-//Globals for tracking state of the GPIO Pin 9-12
-//Located on GPIO1, Pin 28, GPIO Pin 60
+//Globals for tracking state of the GPIO Pin 8-14
+//Located on GPIO0, Pin26, GPIO Pin 26
 //
-static unsigned int ledPin = 60;      //pin 9-12
-static unsigned int ledState = 0;     //state 0 - off, 1 - on
+static unsigned int ledBluePin = 26;      //pin 8-14
+static unsigned int ledBlueState = 0;     //state 0 - off, 1 - on
 
+static unsigned int ledRedPin = 27;      //pin 8-14
+static unsigned int ledRedState = 0;     //state 0 - off, 1 - on
 
 
 ///////////////////////////////////////////
@@ -48,12 +58,19 @@ static DECLARE_DELAYED_WORK(my_work, my_work_function);
 //
 static void my_work_function(struct work_struct *unused)
 {
-   if (!ledState)
-      ledState = 1;
+   if (!ledBlueState)
+   {
+      ledBlueState = 1;
+      ledRedState = 0;      
+   }
    else
-      ledState = 0;
+   {
+      ledBlueState = 0;
+      ledRedState = 1;
+   }
 
-   gpio_set_value(ledPin, ledState);
+   gpio_set_value(ledBluePin, ledBlueState);
+   gpio_set_value(ledRedPin, ledRedState);
 
    schedule_delayed_work(&my_work, 1*HZ);
 }
@@ -68,15 +85,24 @@ static int __init blink2_init(void)
 {
    printk(KERN_INFO "blink2_init\n");
  
-   //set up pin 9 - 12 and set the initial state to off
-   ledState = 0;
-   gpio_request(ledPin, "my_label_gpio_pin_60_is_assigned_to_p9_12");
-   gpio_direction_output(ledPin, ledState);   // ste to output and intial state
-   gpio_set_value(ledPin, ledState);          // Not required as set by line above (here for reference)
+   //set up pins 8-14 and 8-17 (GPIO26 and 27) and set initial
+   //state to off
+   ledBlueState = 0;
+   ledRedState = 0;
+
+   gpio_request(ledBluePin, "label_gpio_pin_26_blue");
+   gpio_request(ledRedPin, "label_gpio_pin_27_red");
+
+   gpio_direction_output(ledBluePin, ledBlueState);   // set to output and intial state
+   gpio_direction_output(ledRedPin, ledRedState);   // set to output and intial state
+
+   gpio_set_value(ledBluePin, ledBlueState);
+   gpio_set_value(ledRedPin, ledRedState);
 
    //export pin to sys/class/gpio.  Pass False to 
    //prevent the pin direction from being changed
-   gpio_export(ledPin, false);             
+   gpio_export(ledBluePin, false);             
+   gpio_export(ledRedPin, false);             
 
    //init work
    schedule_delayed_work(&my_work, 1*HZ);
@@ -92,9 +118,13 @@ static void __exit blink2_exit(void)
 {
    printk(KERN_INFO "blink2_exit\n");
 
-   gpio_set_value(ledPin, 0);              // Turn the LED off, makes it clear the device was unloaded
-   gpio_unexport(ledPin);                  // remove ledPin from sys/class/gpio
-   gpio_free(ledPin);                      // Free the LED GPIO
+   gpio_set_value(ledBluePin, 0);              // Turn the LED off, makes it clear the device was unloaded
+   gpio_unexport(ledBluePin);                  // remove ledPin from sys/class/gpio
+   gpio_free(ledBluePin);                      // Free the LED GPIO
+
+   gpio_set_value(ledRedPin, 0);              // Turn the LED off, makes it clear the device was unloaded
+   gpio_unexport(ledRedPin);                  // remove ledPin from sys/class/gpio
+   gpio_free(ledRedPin);                      // Free the LED GPIO
 
    //remove all items associated with the workqueue
    cancel_delayed_work(&my_work);
